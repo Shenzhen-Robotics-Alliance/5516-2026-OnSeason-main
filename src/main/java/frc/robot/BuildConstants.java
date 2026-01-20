d85f3fe2195e06e890ce717f45e7bdd3a616ff13
Ltcc0 <3381983963@qq.com>
2026-01-20 21:09:03 +0800

    constants update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "5516-2026-OnSeason-main";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 3;
-    public static final String GIT_SHA = "3eb930afbcc8782042deb40004003d84da61c8ba";
-    public static final String GIT_DATE = "2026-01-15 06:41:12 EST";
+    public static final int GIT_REVISION = 4;
+    public static final String GIT_SHA = "df4d151dad0f7e3750801890031ff74d45f52f94";
+    public static final String GIT_DATE = "2026-01-15 06:57:31 EST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-01-15 06:47:38 EST";
-    public static final long BUILD_UNIX_TIME = 1768477658102L;
+    public static final String BUILD_DATE = "2026-01-15 09:53:55 EST";
+    public static final long BUILD_UNIX_TIME = 1768488835293L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
