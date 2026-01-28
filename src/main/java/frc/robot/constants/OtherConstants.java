edb16727834c9a25965d89f9499370a4308b76dc
Milton.Wu <810964823@qq.com>
2026-01-28 20:18:35 +0800

    Recover the intake source 1bde5de

---
+++ src/main/java/frc/robot/constants/OtherConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+import static edu.wpi.first.units.Units.RPM;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+
+public final class OtherConstants {
+    public static class KrakenX60 {
+        public static final AngularVelocity kFreeSpeed = RPM.of(6000);
+    }
+}
