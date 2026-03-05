3a1a7e447205ae571bb596d6136eaa95e1aee9c3
ikun-jntm <1273582130@qq.com>
2026-03-05 20:58:57 +0800

    new parameters

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "5516-2026-OnSeason-main";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 41;
-    public static final String GIT_SHA = "51c3185da79b5f7f0fb28052e2b95f1e2f1c4c79";
-    public static final String GIT_DATE = "2026-03-04 08:36:33 EST";
-    public static final String GIT_BRANCH = "newClimb";
-    public static final String BUILD_DATE = "2026-03-04 20:40:30 EST";
-    public static final long BUILD_UNIX_TIME = 1772674830486L;
+    public static final int GIT_REVISION = 46;
+    public static final String GIT_SHA = "d8ef399f4826acf3c080b9806eb2b71d5cf8fc3c";
+    public static final String GIT_DATE = "2026-03-04 20:45:50 EST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2026-03-05 07:54:49 EST";
+    public static final long BUILD_UNIX_TIME = 1772715289093L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
