59d7375c49d85c83b9a4702294536bd942d5a5f4
Ltcc0 <3381983963@qq.com>
2026-03-12 16:17:33 +0800

    fix auto and visiom

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "5516-2026-OnSeason-main";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 94;
-    public static final String GIT_SHA = "2d89d73ecc50f77b2bc26073fbe02f6d2a45f70e";
-    public static final String GIT_DATE = "2026-03-10 00:57:36 EDT";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-03-10 03:39:56 EDT";
-    public static final long BUILD_UNIX_TIME = 1773128396265L;
+    public static final int GIT_REVISION = 95;
+    public static final String GIT_SHA = "acd44f667f6ea1c6bd4b6a53125f9983f63d57ad";
+    public static final String GIT_DATE = "2026-03-10 04:21:00 EDT";
+    public static final String GIT_BRANCH = "newRpm";
+    public static final String BUILD_DATE = "2026-03-10 07:12:06 EDT";
+    public static final long BUILD_UNIX_TIME = 1773141126257L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
