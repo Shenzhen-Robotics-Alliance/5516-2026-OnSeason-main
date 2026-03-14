afe8c8c815646571581ff9fc95f0933c0aea3167
Ltcc0 <3381983963@qq.com>
2026-03-14 23:43:11 +0800

    new Auto Path For ToHP

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "5516-2026-OnSeason-main";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 106;
-    public static final String GIT_SHA = "6183c5951630a0611e011b62eca7aabd7395d6fb";
-    public static final String GIT_DATE = "2026-03-13 20:11:07 EDT";
-    public static final String GIT_BRANCH = "newAutoOrigin";
-    public static final String BUILD_DATE = "2026-03-13 22:13:35 EDT";
-    public static final long BUILD_UNIX_TIME = 1773454415819L;
+    public static final int GIT_REVISION = 107;
+    public static final String GIT_SHA = "4c7f7a89648dd5975200480c1ef4dad3964783fb";
+    public static final String GIT_DATE = "2026-03-14 00:28:42 EDT";
+    public static final String GIT_BRANCH = "fixAutoError";
+    public static final String BUILD_DATE = "2026-03-14 11:41:52 EDT";
+    public static final long BUILD_UNIX_TIME = 1773502912390L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
